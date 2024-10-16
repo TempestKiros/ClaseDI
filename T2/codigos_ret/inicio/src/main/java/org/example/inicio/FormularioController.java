12cc6760609cf88a8e1d26020f4afa7f44a190ba
Deep Feeder <gamma.deep.feeders@gmail.com>
2024-10-16 15:28:18 +0200

    ff

--- T2/codigos_ret/inicio/src/main/java/org/example/inicio/FormularioController.java
+++ T2/codigos_ret/inicio/src/main/java/org/example/inicio/FormularioController.java
@@ -1,26 +1,66 @@
 package org.example.inicio;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FormularioController implements Initializable {
+public class FormularioController implements Initializable,
+        EventHandler<ActionEvent> {
 
     @FXML
     private Button bEnviar, bLimpiar, bValidar;
     @FXML
     private TextField editNombre, editApellido, editTelefono, editCorreo;
     @FXML
-    private FlowPane layoutBotones;
+    private FlowPane layoutBotones,panelIzquierdo,panelDerecha;
+    @FXML
+    private BorderPane panelGeneral;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)  {
         // ejecutado cuando fxmlloader.load()
+        System.out.println("Ejecutada controladora");
+        acciones();
+        panelGeneral.setLeft(null);
+        panelGeneral.setRight(null);
+    }
 
+    private void acciones() {
+        bEnviar.setOnAction(this);
+        bLimpiar.setOnAction(this);
+        bValidar.setOnAction(this);
+        bEnviar.setOnMouseEntered(new MouseHandler());
+        bEnviar.setOnMouseExited(new MouseHandler());
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+        if (actionEvent.getSource() == bEnviar){
+            System.out.println("Pulsado enviar");
+        } else if (actionEvent.getSource() == bLimpiar) {
+            System.out.println("Pulsado limpiar");
+        } else if (actionEvent.getSource() == bValidar) {
+            System.out.println("Pulsado validar");
+        }
+    }
+
+    class MouseHandler implements EventHandler<MouseEvent>{
+
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            System.out.println("Raton detectado");
+        }
     }
 }
